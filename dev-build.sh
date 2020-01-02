f342b1284a7b5a1073f52e89f192f808f725d7d3
RonnyDo <ronnydobra at arcor de>
2020-01-02 19:26:51 +0100

    fix build management

--- dev-build.sh
+++ dev-build.sh
@@ -11,5 +11,8 @@ ninja
 # Is this necessary to install al languages?
 #sudo ninja install
 
+sudo cp ../data/com.github.ronnydo.wammer.gschema.xml /usr/share/glib-2.0/schemas/
+sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
+
 G_MESSAGES_DEBUG=all ./com.github.ronnydo.wammer
 cd ..
